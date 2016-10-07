e283b4bf6c40fa6f25969212aae02e090e2bfd96
Test <a@1.com>
2016-10-07 14:44:32 +0100

    Intial Pyramid

---
+++ Pyramid/Pyramid/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = 0;
+            bool valid = false;
+
+            while (!valid)
+            {
+                Console.Write("Enter the pyramid height: ");
+                string currentEntry = Console.ReadLine();
+                valid = Int32.TryParse(currentEntry, out height);
+
+                if (height <= 0 || height > 23)
+                    valid = false;
+            }
+                    
+            // Print the pyramid
+            //height+1
+            int hashCount = 2;
+            for (int i = 0; i < height; i++)
+            {
+                string line = "";
+                int spaces = height - hashCount + 1;
+                for (int j = 0; j < spaces; j++)
+                {
+                    line += " ";
+                }
+
+                for (int j = 0; j < hashCount; j++)
+                {
+                    line += "#";
+                }
+
+                Console.WriteLine(line);
+                hashCount++;
+
+            }
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
